9e163c3b1a17e3b598e396a91adc40c1f18f7649
[Andrew Skvarcius] <andrewskvarcius@gmail.com>
2023-08-31 13:42:57 -0400

    Markov Machine

--- makeText.js
+++ makeText.js
@@ -1,2 +1,44 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const markov = require("./markov");
+const axios = require("axios");
+const process = require("process");
 
+
+function generateText(txt){
+    let mark  = new markov.MarkovMachine(txt);
+    console.log(mark.makeTxt())
+}
+
+function makeTxt(path){
+    fs.readFile(path, "utf8", function cb(err, data){
+        if (err){
+            console.log(`File not readable: ${path}: ${err}`);
+            process.exit(1);
+        } else{
+            generateText(data);
+        }
+    })
+}
+async function makeUrlText(url){
+    let response;
+    try{
+        response = await axios.get(url);
+
+    } catch (err){
+        console.error(`Error getting url: ${url}; ${err}`)
+        process.exit(1);
+    }
+    generateText(response.data);
+}
+let [method , path] = process.argv.slice(2);
+if (method === "file"){
+ makeTxt(path);
+}
+else if (method === "url"){
+    makeUrlText(path);
+    }
+else{
+    console.error(`Unknown method: ${method}`)
+    process.exit(1);
+}    
--- markov.js
+++ markov.js
@@ -18,12 +18,38 @@ class MarkovMachine {
 
   makeChains() {
     // TODO
-  }
+    let chains = new Map();
 
+    for(let i=0; i< this.words.length; i+= 1){
+      let wrd = this.words[i];
+      let nxtwrd = this.words[i+1] || null;
+
+      if (chains.has(wrd)) chains.get(wrd).push(nxtwrd);
+      else chains.set(wrd, [nxtwrd]);
+
+    }
+    this.chains = chains;
+  }
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
+  }
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
+  makeTxt(numWords = 100) {
     // TODO
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    while (out.length < numWords && key != null){
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    return out.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
